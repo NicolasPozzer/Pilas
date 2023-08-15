97b7f605e982b146ab6b2faf5b18c58f161b5a9b
NicolasPozzer <pozzernico@gmail.com>
2023-08-15 17:59:55 -0300

    publish-in-git

---
+++ src/pilas/Pilas.java
@@ -0,0 +1,36 @@
+
+package pilas;
+
+import java.util.Stack;
+
+public class Pilas {
+    
+    public static void main(String[] args) {
+        
+        Stack<Integer> pila = new Stack<Integer>();
+        
+        System.out.println("Lista vacia: "+ pila);
+        System.out.println("Esta vacia? "+ pila.isEmpty());
+        
+        //agregar elementos
+        pila.push(1);
+        pila.push(2);
+        pila.push(3);
+        pila.push(4);
+        pila.push(5);
+        
+        
+        //recorrido por foreach porque tiene el ":"
+        for(Integer pilita : pila){
+            System.out.println("pilita");
+        }
+        
+        //mostrar pila
+        System.out.println("Pila: "+ pila);
+        System.out.println("Pila vacia? "+ pila.isEmpty());
+        
+        pila.pop(); //eliminar el ultimo registro que entro
+        System.out.println("Esta el 3?" + pila.search(3));
+        System.out.println("Ultimo agregado: "+ pila.peek());
+    }   
+}
